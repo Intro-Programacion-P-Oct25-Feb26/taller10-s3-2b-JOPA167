d8c97858ad45c4e340d33259c55bb2b7aa8796c8
JOPA167 <joparajirj@gmail.com>
2025-12-18 12:32:45 -0500

    solucion problema 5

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,26 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+
+        };
+        String inicial;
+        String reporte = "";
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int c = 0; c < estudiantes[i].length; c++) {
+                inicial = estudiantes[i][c].substring(0,1);
+                if (inicial.equals("S")||inicial.equals("T")||inicial.equals("P")){
+                    reporte = String.format("%s%s ",reporte,estudiantes[i][c]);
+                }
+            }
+        }
+        System.out.printf("%s\n",reporte);
+
+    }
 }
