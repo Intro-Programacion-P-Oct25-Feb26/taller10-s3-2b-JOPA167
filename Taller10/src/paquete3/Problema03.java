f68d7dd99df8e150ef503a34aaf32d6631f7fbe8
JOPA167 <joparajirj@gmail.com>
2025-12-18 12:10:38 -0500

    solucion problema 4

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,46 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
+
+    public static void main(String[] args) {
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}
+        };
+        String[] zonas = {"Ana", "Luis", "Carlos", "María"};
+        int[] funcional = new int[4];
+        int zonasFuncionando = 0;
     
+        String reporte = "";
+        int fallos = 0;
+        int sinFallos = 0;
+
+        for (int i = 0; i < sensores.length; i++) {
+            zonasFuncionando = 0;
+            for (int c = 0; c < sensores[i].length; c++) {
+                if (sensores[i][c]) {
+                    zonasFuncionando = zonasFuncionando + 1;
+                }
+                funcional[i] = zonasFuncionando;
+            }
+        }
+
+        for (int i = 0; i < sensores.length; i++) {
+            if (funcional[i] == 6) {
+                fallos = fallos + 1;
+                
+            } else {
+                sinFallos = sinFallos + 1;
+            }
+            
+            reporte = String.format("El total de zonas sin fallos es: %d\n"
+                    + "El total de zonas con fallos es: %d",
+                    fallos,
+                    sinFallos);
+
+        }
+        System.out.printf("%s\n", reporte);
+    }
 }
--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,95 @@
  */
 package paquete4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        String reporteA = "";
+        String reporteB = "";
+        String reporteFinal;
+        boolean primera= true;
+        boolean segunda = false;
+
+        int[][] a = new int[3][2];
+        int[][] b = new int[3][2];
+        System.out.println("MATRIZ A");
+        for (int filaA = 0; filaA < a.length; filaA++) {
+            for (int colA = 0; colA < a[filaA].length; colA++) {
+                System.out.printf("Ingrese el valor para la posicion (%d-%d) de "
+                        + "la matriz A: ",
+                        filaA,
+                        colA);
+                a[filaA][colA] = entrada.nextInt();
+                
+            }
+        }
     
+        for (int i = 0; i< a.length;i++){
+            for (int c = 0; c< a[i].length;c++){
+                reporteA = String.format("%s%d\t",
+                        reporteA,
+                        a[i][c]);
+            }
+            reporteA = String.format("%s\n",reporteA);
+        }
+        
+        System.out.println("-----------------------------");
+        
+        System.out.println("MATRIZ B");
+        for (int filaB = 0; filaB < b.length; filaB++) {
+            for (int colB = 0; colB < b[filaB].length; colB++) {
+                System.out.printf("Ingrese el valor para la posicion (%d-%d) de "
+                        + "la matriz B: ",
+                        filaB,
+                        colB);
+                b[filaB][colB] = entrada.nextInt();
+                
+            }
+        }
+        
+        for (int i = 0; i< b.length;i++){
+            for (int c = 0; c< a[i].length;c++){
+                reporteB = String.format("%s%d\t",
+                        reporteB,
+                        b[i][c]);
+            }
+            reporteB=String.format("%s\n",reporteB);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+
+
+                if (a[i][j] < b[i][j]) {
+                    primera = false;
+                } else {
+                    if (a[i][j] > b [i][j]) {
+                        segunda = true;
+                    }
+
+                }
+            }
+
+        }
+        if (primera == true && segunda == true){
+            reporteFinal = String.format("La matriz A es mayor que la matriz B");
+        }else{
+            reporteFinal = String.format("La matriz A no es mayor que la matriz B");
+        }
+        reporteFinal = String.format("\n--MATRIZ A--\n%s\n--MATRIZ B--\n%s\n\n%s", 
+                reporteA,
+                reporteB,
+                reporteFinal);
+        
+        System.out.printf("%s\n",reporteFinal);
+    }
 }
